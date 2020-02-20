ea8e1814496f8640ded7cb57303035d7fcc943e9
Ghezlene Doballah <ghyzounette@gmail.com>
2020-02-20 16:33:25 +0100

    commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+ $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+ $modify = 'tempor';
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php badwords</title>
+  </head>
+  <body>
+  <p><?php echo $text; ?></p>
+  <p>lunghezza testo: <?php echo strlen($text);?> carrateri</p>
+  <p><?php echo str_replace($modify, '***',$text  ); ?></p>
+
+  </body>
+</html>
